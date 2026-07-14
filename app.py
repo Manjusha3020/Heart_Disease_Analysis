a3f485c71295b4c1f649178b58599e07fa62f957
Manjusha3020 <klakshmimanjusha2004@gmail.com>
2026-07-14 22:59:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+<<<<<<< HEAD
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/story')
+def story():
+    return render_template('base.html')
+
+if __name__ == '__main__':
+=======
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/story')
+def story():
+    return render_template('base.html')
+
+if __name__ == '__main__':
+>>>>>>> 23b7505c4038c2702db7b8ec8a7c28469790f6b9
+    app.run(debug=True)
